a1f4dda9af5776c2911bc571ba9703ca9ed9d973
Leisy Ladino <leisy95.ladino@gmail.com>
2025-08-29 18:44:59 -0500

    Agregar Url en Environment

--- Frontend/permisos-app/src/environments/environment.prod.ts
+++ Frontend/permisos-app/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://project-solicitud-permisos.onrender.com/api'
+  apiUrl: 'https://project-solicitud-permisos.up.railway.app/api'
 };
