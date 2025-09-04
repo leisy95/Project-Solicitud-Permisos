5dad6d340cbe98ed671d886adc37f1b1a3700ce0
Leisy Ladino <leisy95.ladino@gmail.com>
2025-09-03 22:05:38 -0500

    Realizando configuracion de responsive para movil admin

--- Frontend/permisos-app/src/app/admin/dashboard/dashboard.component.ts
+++ Frontend/permisos-app/src/app/admin/dashboard/dashboard.component.ts
@@ -8,12 +8,19 @@ import { Router } from '@angular/router';
 })
 export class DashboardComponent {
 
-  constructor(private router: Router){}
+  constructor(private router: Router) { }
+
+  isCollapsed = localStorage.getItem('sidebarCollapsed') === '1';
+
+  toggleSidebar() {
+    this.isCollapsed = !this.isCollapsed;
+    localStorage.setItem('sidebarCollapsed', this.isCollapsed ? '1' : '0');
+  }
 
 
-logout(): void {
-  localStorage.removeItem('token');
-  localStorage.removeItem('rol'); // si guardaste el rol
-  this.router.navigate(['/login']);
-}
+  logout(): void {
+    localStorage.removeItem('token');
+    localStorage.removeItem('rol'); // si guardaste el rol
+    this.router.navigate(['/login']);
+  }
 }
